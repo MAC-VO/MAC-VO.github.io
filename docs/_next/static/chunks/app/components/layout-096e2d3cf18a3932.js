569bfffd76ee7ae182aa3f2a700f3a03a044868e
Yutian Chen <markchenyutian@gmail.com>
2024-07-19 14:41:10 -0400

    update nojekyll in postbuild process

--- docs/_next/static/chunks/app/components/layout-096e2d3cf18a3932.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[686],{2621:function(n,e,u){Promise.resolve().then(u.t.bind(u,5463,23))},5463:function(){}},function(n){n.O(0,[494,48,225,744],function(){return n(n.s=2621)}),_N_E=n.O()}]);
--- docs/_next/static/chunks/app/layout-b2a3ae177d2af6fa.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{393:function(n,e,u){Promise.resolve().then(u.t.bind(u,271,23))},271:function(){}},function(n){n.O(0,[519,48,225,744],function(){return n(n.s=393)}),_N_E=n.O()}]);
